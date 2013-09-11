8bb2d62e79ed7a398d6fa8b7ae083b9ba613b552
Patrick Tombez <patrick.tombez@blues2.ch>
2013-09-11 17:28:16 +0200

    Fix mismatch function name Add debug messages
    wip

--- bss_duration_widget/parser.py
+++ bss_duration_widget/parser.py
@@ -28,13 +28,11 @@ class bss_parser(osv.TransientModel):
     _name = "bss_duration_widget.bss_parser"
     _description = "Parser and validator for duration"
 
-    @staticmethod
     def _validate_str(self, str_to_validate):
         rex = r'^[0-9]{1,}d$|^[0-9]{1,}h?$|^[0-9]{1,}d\ [0-9]{1,2}h$'
         
         return match(rex, str_to_validate) is not None
-    
-    @staticmethod
+
     def parse_str(self, str_to_parse):
         if not self._validate_str(str_to_parse):
             raise osv.except_osv("Validation error", "String duration is not valid !")
@@ -50,11 +48,12 @@ class bss_parser(osv.TransientModel):
         
         return td.total_seconds()
 
-    @staticmethod
     def display_value(self, value_to_display):
+        print("Calling display_value")
         if type(value_to_display) in [str,int]:
             value_to_display = float(value_to_display)
         elif type(value_to_display) == datetime:
             value_to_display = float((value_to_display-datetime(1970,1,1)).total_seconds())
         
         return "%sd %sh" % (int(value_to_display//(60.0*60.0*24.0)),int(value_to_display%(60.0*60.0*24.0)))
+bss_parser()
--- bss_duration_widget/static/src/js/resource.js
+++ bss_duration_widget/static/src/js/resource.js
@@ -1,26 +1,24 @@
 openerp.duration = function(instance)
-{
-	console.log("Module loaded");
-	
+{	
 	instance.duration.FieldDuration = instance.web.form.FieldChar.extend(
-	    {
-	    	template : "FieldDuration",
-	    	init: function (view, code)
-	    	{
-	    		this._super(view, code);
-	        	console.log('loading...');
-	    	},
-	    	render_value: function()
-	    	{
-	    		var parser_obj = new instance.web.Model('bss_duration_widget.bss_parser');
-	    		parser_obj.call('display_value', this.get('value')).then(function(result)
-	    			{
-	    				var content = $("div#duration_widget_content");
-	    				content.empty();
-	    				content.html("<input type='text' value='" + result + "' />");
-	    			});
-	    	}
-	    });
+    {
+    	template : "FieldDuration",
+    	initialize_content: function()
+    	{
+    		this._super();
+    	},
+    	
+    	render_value: function()
+    	{
+    		var parser_obj = new instance.web.Model('bss_duration_widget.bss_parser');
+    		parser_obj.call('display_value', this.get('value')).then(function(result)
+    			{
+    				var content = $("div#duration_widget_content");
+    				content.empty();
+    				content.append("<input type='text' value='" + result + "' />");
+    			});
+    	}
+    });
 	    
 	instance.web.form.widgets.add('duration', 'instance.duration.FieldDuration');
 }
