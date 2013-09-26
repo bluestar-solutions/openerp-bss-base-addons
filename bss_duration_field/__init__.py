45ea5afdcc14813e7b1681e5447acaeaf0f8ae70
Patrick Tombez <patrick.tombez@blues2.ch>
2013-09-26 14:12:58 +0200

    Rename file in order to match its purpose Update init file to reflect filename changes

--- bss_duration_field/__init__.py
+++ bss_duration_field/__init__.py
@@ -19,6 +19,6 @@
 #
 ##############################################################################
 
-import parser
+import duration_field
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
