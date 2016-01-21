7ba1a01d31c8be4d40daf77146c57319a269f313
Daniel Le Gall <daniel.legall@blues2.ch>
2016-01-21 10:21:21 +0100

    add persistent session module

---
+++ bss_persistent_session/static/js/call.js
@@ -0,0 +1,19 @@
+openerp.bss_persistent_session = function(instance) {
+    	   $(document).ready(function(){
+               var P = new instance.web.Model('ir.config_parameter');
+               var parentDelay = 0;
+               var sessionCallback = function(){
+           	    P.call('get_param', ['bss_persistent_session.delay']).then(function(delay){
+           		if(parentDelay != delay) {
+                   	    	clearInterval(sessionback);
+                   		parentDelay = delay;
+                   		sessionback = setInterval(sessionCallback, delay);
+           		}
+           	    }, function(){clearInterval(sessionback)});
+           	};
+               sessionback = setInterval(sessionCallback,parentDelay);
+    	   });
+               
+               
+               
+};
