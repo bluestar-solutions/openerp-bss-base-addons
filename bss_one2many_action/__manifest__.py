21245198149b77410ec9f1e62b80af23c92fa474
Hervé Martinet <herve.martinet@bluestar.solutions>
2017-10-17 14:35:44 +0200

    Add a module for nre widget one2many_action

---
+++ bss_one2many_action/__manifest__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013-2015 Bluestar Solutions Sàrl (<http://www.blues2.ch>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'One2Many Action Widget',
+    'version': '7.0.2.3-20160509',
+    "category": 'Bluestar/Generic module',
+    'complexity': "easy",
+    'description': """
+One2Many Action Widget
+======================
+
+A widget for one2many field which you can customizethe action when a user click
+on a line in list.
+
+<field name="my_one2many_ids" widget="one2many_action"
+    context="{'on_click_action': 'my_object_method'}">[…]</field>
+
+If on_click_action is not defined, no action will be called on click.
+    """,
+    'author': 'Bluestar Solutions Sàrl',
+    'website': 'http://www.blues2.ch',
+    'depends': ['web'],
+    'data': ['views/assets_backend_template.xml'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
