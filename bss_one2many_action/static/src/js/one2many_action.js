391ab1bd138759a569da3e446c272da58075af30
Hervé Martinet <herve.martinet@bluestar.solutions>
2017-11-06 14:35:14 +0100

    Fix bug if component is used multiple times on same view.
    Take the conext at init phase (not in the click listener), and put it
    in an object indexed by the field name, retrive it by field
    name in the listener because it is only one instance of the widget for
    all fields on a form.

--- bss_one2many_action/static/src/js/one2many_action.js
+++ bss_one2many_action/static/src/js/one2many_action.js
@@ -6,15 +6,17 @@ odoo.define('bss_one2many_action.FieldOne2ManyAction', function (require) {
     var FieldOne2Many = core.form_widget_registry.get('one2many');
 
     var FieldOne2ManyAction = FieldOne2Many.extend({
+        _actions: {},
         init: function() {
             this._super.apply(this, arguments);
+            var context = this.build_context().eval();
+            this._actions[this.name] = context['on_click_action'];
             self = this;
             this.x2many_views.list.include({
                 do_activate_record: function(index, id) {
-                    var context = self.build_context().eval();
-                    if (context['on_click_action']) {
+                    if (self._actions[this.x2m.name]) {
                         this.do_button_action(
-                            context['on_click_action'], id, function () {});
+                            self._actions[this.x2m.name], id, function () {});
                     }
                 }
             });
