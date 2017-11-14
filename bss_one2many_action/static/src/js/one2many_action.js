5e2f9799834c5d45f52591c09a63661ebfc097ae
Hervé Martinet <herve.martinet@bluestar.solutions>
2017-11-14 09:06:20 +0100

    Fix bug with many one2many widget on same view.
    Check the widget type of click event and call super if
    it is not a one2many_action.

--- bss_one2many_action/static/src/js/one2many_action.js
+++ bss_one2many_action/static/src/js/one2many_action.js
@@ -5,6 +5,12 @@ odoo.define('bss_one2many_action.FieldOne2ManyAction', function (require) {
     var core = require('web.core');
     var FieldOne2Many = core.form_widget_registry.get('one2many');
 
+    /**
+     * Create a FieldOne2ManyAction, based on FieldOne2Many.
+     * extend() of Odoo Class will call extend() function of underscore
+     * library but with create a new object before. Then FieldOne2Many
+     * is not affected.
+     */
     var FieldOne2ManyAction = FieldOne2Many.extend({
         _actions: {},
         init: function() {
@@ -14,9 +20,19 @@ odoo.define('bss_one2many_action.FieldOne2ManyAction', function (require) {
             self = this;
             this.x2many_views.list.include({
                 do_activate_record: function(index, id) {
-                    if (self._actions[this.x2m.name]) {
-                        this.do_button_action(
-                            self._actions[this.x2m.name], id, function () {});
+                    // x2many_views is a reference shared by all x2m
+                    // components, then we have to check if the
+                    // event is from a one2many_action, otherwise
+                    // calling super to not affect other component
+                    // if there is many x2m components on the same view.
+                    if (this.x2m.widget == 'one2many_action') {
+                        if (self._actions[this.x2m.name]) {
+                            this.do_button_action(
+                                self._actions[this.x2m.name], id,
+                                function () {});
+                        }
+                    } else {
+                        this._super(index, id);
                     }
                 }
             });
