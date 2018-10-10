8aa7053de500f922cc4cd81b1dcffeb6b65256fa
Pierre-Yves Berger <pierre-yves.berger@bluestar.solutions>
2018-10-10 16:22:49 +0200

    Add function to ge quarter start date

--- bss_utils/utils/dateutils.py
+++ bss_utils/utils/dateutils.py
@@ -2,7 +2,7 @@
 # Part of Python Utilities.
 # See LICENSE file for full copyright and licensing details.
 
-from datetime import datetime
+from datetime import datetime, date
 import time
 
 ORM_DATE_FORMAT = '%Y-%m-%d'
@@ -41,6 +41,11 @@ def datetime2timestamp(value, millis=False):
 def timestamp2datetime(value, millis=False):
     """Return a datetime from a int timestamp (with or without millisecond)"""
     return datetime.fromtimestamp(value / (millis and 1000. or 1))
+
+
+def get_quarter_start(date_at):
+    quarter_month = ((date_at.month - 1) / 3) * 3 + 1
+    return date(date_at.year, quarter_month, 1)
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
